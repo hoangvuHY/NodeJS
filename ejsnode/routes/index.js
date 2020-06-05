ba27f9b8c872721aec1541e5dfc53ce766933a8a
Vuoppa <vuoppa@example.net>
2020-06-05 14:50:47 +0700

    Truyen tham so thong qua router

--- ejsnode/routes/index.js
+++ ejsnode/routes/index.js
@@ -11,8 +11,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 /* GET sanpham page. */
-router.get('/sanpham', function (req, res, next) {
-  res.render('sanpham', { title: 'San pham' });
+router.get('/sanpham/:chisosanpham-:gia', function (req, res, next) {
+  res.send("Mã sản phẩm là: " + req.params.chisosanpham + " Giá sản phẩm là: " + req.params.gia);
 });
 /* GET tintuc page. */
 router.get('/tintuc', function (req, res, next) {
