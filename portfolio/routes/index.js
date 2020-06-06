91fa03f604c4226626caa8f6ded38eea8822a052
Vuoppa <vuoppa@example.net>
2020-06-06 07:25:47 +0700

    xong phan khoi tao tu html sang ejs

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET detail. */
+router.get('/chi-tiet-san-pham.html', function (req, res, next) {
+  res.render('chi-tiet-san-pham', { title: 'Express' });
 });
 
 module.exports = router;
