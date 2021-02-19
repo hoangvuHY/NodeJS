d7d2d2cf1b25c6428c100f1f4cf0c3e2e34a1c16
Vu Oppa <hoangvu15499@gmail.com>
2021-02-19 20:16:17 +0700

    Socket.io

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var divisor = document.getElementById("divisor"),
-    handle = document.getElementById("handle"),
-    slider = document.getElementById("slider");
+  handle = document.getElementById("handle"),
+  slider = document.getElementById("slider");
 
 function moveDivisor() {
-    handle.style.left = slider.value + "%";
-    divisor.style.width = slider.value + "%";
+  handle.style.left = slider.value + "%";
+  divisor.style.width = slider.value + "%";
 }
 
-window.onload = function() {
-    moveDivisor();
+window.onload = function () {
+  moveDivisor();
 };
